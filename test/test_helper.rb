8ec09f16a7ec9604a3debd89647186b2b70f149b
Matt Griffin <matt@griffinonline.org>
2009-05-27 09:58:08 -0400

    Allow specification of rails gem version used during testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,13 +6,15 @@ require 'mocha'
 ENV["RAILS_ENV"] = "test"
 RAILS_ROOT = "anywhere"
 
+gem 'rails', ENV['RAILS_GEM'] if ENV['RAILS_GEM']
+
 require 'active_support'
 require 'action_controller'
 require 'action_controller/test_case'
 require 'action_controller/test_process'
 
-I18n.load_path << File.join(File.dirname(__FILE__), 'locales', 'en.yml')
-I18n.reload!
+#I18n.load_path << File.join(File.dirname(__FILE__), 'locales', 'en.yml')
+#I18n.reload!
 
 # Add IR to load path and load the main file
 ActiveSupport::Dependencies.load_paths << File.expand_path(File.dirname(__FILE__) + '/../lib')
